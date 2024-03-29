3a0df0a2fe1cdb185c3a1efcda5852b307b0d8c2
hex benjamin <hex@hexbenjam.in>
2024-03-28 22:18:21 -0400

    cloud scrolling!!!!!

--- tcott/tcott.py
+++ tcott/tcott.py
@@ -27,36 +27,58 @@ def background():
 class Cloud:
     def __init__(self, index: int):
         self.index: int = index
+
+        if self.index > 13:
+            raise ValueError(
+                "Cloud index must be less than 14. We only got 14 clouds. Sorry, homie pal."
+            )
+
         self.image: str = f"clouds/cloud_{str(self.index).zfill(2)}.png"
-        self.nudge = "-33%" if self.index % 2 == 0 else "33%"
+        # self.nudge = "33%" if self.index % 2 == 0 else "-33%"
+        self.side = "end" if self.index % 2 == 0 else "start"
+        self.nudge = "33%" if self.index % 2 == 0 else "-33%"
 
     def render(self) -> rx.Component:
         return rx.image(
             src=self.image,
             style={
                 "opacity": "0.5",
-                "width": "50vw",
+                "mix-blend-mode": "screen",
+                "width": "69vw",
                 "height": "auto",
+                "align-self": self.side,
                 "transform": f"translateX({self.nudge})",
+                # "border": "1px solid blue",
             },
         )
 
 
-def cloud_column(cloud_count: int = 1):
+def cloud_column(cloud_count: int = 1, cloud_seconds: int = 12):
     return rx.flex(
         *[Cloud(i).render() for i in range(cloud_count)],
         direction="column",
+        spacing="7",
+        style={
+            "animation-name": "cloudscroll",
+            "animation-duration": f"{cloud_count * cloud_seconds}s",
+            "animation-timing-function": "linear",
+            "width": "100%",
+            # "border": "1px solid red",
+        },
     )
 
 
 @rx.page(route="/", title="The Changing of the Trees")
 def landing() -> rx.Component:
     """The landing page."""
-    cloud1 = Cloud(1)
-    return rx.container(
+    return rx.box(
         background(),
-        cloud_column(3),
-        class_name="full-screen",
+        cloud_column(14),
+        style={
+            "overflow": "hidden",
+            "width": "100vw",
+            "height": "100vh",
+        },
     )
 
 
